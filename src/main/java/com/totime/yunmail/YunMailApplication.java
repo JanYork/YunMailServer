71cce31d8486010ffd12c927f9679eccf668a169
JanYork <747945307@qq.com>
2022-12-23 12:48:29 +0800

    初始化云寄服务端项目

---
+++ src/main/java/com/totime/yunmail/YunMailApplication.java
@@ -0,0 +1,13 @@
+package com.totime.yunmail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YunMailApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YunMailApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/totime/yunmail/YunMailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.totime.yunmail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YunMailApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
