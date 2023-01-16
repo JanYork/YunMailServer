956aa7cadd3258d96f3587cfce1eaacbe54fa28d
JanYork <747945307@qq.com>
2023-01-16 22:25:01 +0800

    refactor：短信枚举类(SDK枚举、签名枚举、模板枚举)新增数据

--- yun-common/src/main/java/cn/totime/common/sms/enums/SdkAppEnum.java
+++ yun-common/src/main/java/cn/totime/common/sms/enums/SdkAppEnum.java
@@ -9,7 +9,11 @@ public enum SdkAppEnum {
     /**
      * 默认短信应用
      */
-    SDK_APP_ID("1400751895", "短信默认应用");
+    SDK_APP_ID("1400751895", "短信默认应用"),
+    /**
+     * 云寄短信应用
+     */
+    YUN_SDK_APP_ID("1400791247", "云寄短信应用");
 
     private final String id;
     private final String desc;
--- yun-common/src/main/java/cn/totime/common/sms/enums/SignNameEnum.java
+++ yun-common/src/main/java/cn/totime/common/sms/enums/SignNameEnum.java
@@ -10,15 +10,25 @@ public enum SignNameEnum {
     /**
      * 默认签名
      */
-    DEFAULT("拾小染网络科技");
+    DEFAULT(504281, "拾小染网络科技"),
+    /**
+     * 与时签名
+     */
+    YUN(510118, "初柒与时同行");
 
     private final String signName;
+    private final Integer id;
 
-    SignNameEnum(String signName) {
+    SignNameEnum(Integer id, String signName) {
         this.signName = signName;
+        this.id = id;
     }
 
     public String getSignName() {
         return signName;
+    }
+
+    public Integer getId() {
+        return id;
     }
 }
