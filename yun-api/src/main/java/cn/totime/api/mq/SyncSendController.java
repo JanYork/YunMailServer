59a87503b657f209f715bd17af2042af148e5d77
JanYork <747945307@qq.com>
2023-03-26 22:45:35 +0800

    重置仓库

--- yun-api/src/main/java/cn/totime/api/mq/SyncSendController.java
+++
@@ -1,9 +0,0 @@
-package cn.totime.api.mq;
-
-/**
- * @author JanYork
- * @date 2023/1/3 13:54
- * @description MQ消息发送控制器(同步)
- */
-public class SyncSendController {
-}
--- yun-app/src/main/resources/templates/email/test.ftl
+++
@@ -1,6 +0,0 @@
-<!DOCTYPE html>
-<html>
-    <body>
-        <h4>${Text}，你好！</h4>
-    </body>
-</html>
